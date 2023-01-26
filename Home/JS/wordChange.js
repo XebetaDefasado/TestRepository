930314226ec4d213ef6c94ce89404bcd8ad7abb1
XebezeraDefasado <minchoste@gmail.com>
2023-01-25 21:02:16 -0300

    Damn, Objects are the synonym of hell

--- Home/JS/wordChange.js
+++ Home/JS/wordChange.js
@@ -14,37 +14,59 @@ button.addEventListener('click', () => {
         return
     }
 
-    function pickRandom(arg){
-        if(Array.isArray(arg)){
-            return arg[Math.floor(Math.random() * arg.length)]
+   
+        function pickRandom(arg){
+            if(Array.isArray(arg)){
+                return arg[Math.floor(Math.random() * arg.length)]
+            }
+            if(typeof arg === "number"){
+                return Math.floor(Math.random() * arg)
+            }
         }
-        if(typeof arg === "number"){
-            return Math.floor(Math.random() * arg)
+
+        function createChecking(){
+            for(let j = 0; j < arguments.length; j++){
+                for(let i = 0; i < arguments[j].length; i++){
+                    let CheckingP = {}
+                   CheckingF.push(Object.defineProperty(CheckingP, arguments[j][i], {value: false, writable: true}))
+                }
+            }
+            return CheckingF
         }
-    }
+    
 
-    //When textbox has text:
 
-    //space = '&nbsp;' AND '\u0020' //- The most fucking important thing, at least at 21:14 1/23/2023
-
-    AditiveConj = [
+    //space = '&nbsp;' AND '\u0020' //- The most fucking important thing, at least at 18:48 1/25/2023
+    let CheckingF = []
+    
+    let AditiveConj = [
         "e",
         "também",
         "como também",
         "além de que",
         "mais"
     ]
+
+    createChecking(AditiveConj)
     
     if(textbox.innerHTML){
-        if(textbox.innerHTML.match(/(&nbsp;|\u0020)\u0065(&nbsp;|\u0020)/) !== null){
-            let matcher = textbox.innerHTML.match(/(&nbsp;|\u0020)\u0065(&nbsp;|\u0020)/)[0]
-            let spaceBef = matcher.slice(0, (matcher.indexOf('\u0065')))
-            let spaceAft = matcher.slice(matcher.indexOf('\u0065') +1, matcher.length)
-            textbox.innerHTML = textbox.innerHTML.replace(matcher, `${spaceBef}${pickRandom(AditiveConj)}${spaceAft}`)
+        function eAditive() {
+            while(textbox.innerHTML.match(/(&nbsp;|\u0020)\u0065(&nbsp;|\u0020)/) !== null){
+                let matcher = textbox.innerHTML.match(/(&nbsp;|\u0020)\u0065(&nbsp;|\u0020)/)[0]
+                let spaceBef = matcher.slice(0, (matcher.indexOf('\u0065')))
+                let spaceAft = matcher.slice(matcher.indexOf('\u0065') +1, matcher.length)
+                textbox.innerHTML = textbox.innerHTML.replace(matcher, `${spaceBef}${pickRandom(AditiveConj)}${spaceAft}`)
+            }
+            CheckingF[0].e = true
+            console.log(CheckingF);
+            return 
         }
+        eAditive()
     }
     
 })
+
+//"Legacy Code" so you can see my suffering in a step-by-step tutorial
 
 /*
 if(textbox.innerHTML.match(/&nbsp;\u0065&nbsp/) !== null){
@@ -52,6 +74,30 @@ if(textbox.innerHTML.match(/&nbsp;\u0065&nbsp/) !== null){
     textbox.innerHTML = textbox.innerHTML.replace(/&nbsp;\u0065&nbsp;/, `&nbsp;${pickRandom(AditiveConj)}&nbsp;`)
 }
 */
+
 /*
 (&nbsp;|\u0020)\u0065(&nbsp;|\u0020)
+*/
+
+//Checking.push(Object.create({i: false}))
+
+/*
+for(let j = 0; j < arguments.length; j++){
+                for(let i = 0; i < arguments[j].length; i++){
+                    Object.defineProperty(Checking, arguments[j][i], {value: false})
+                }
+            }
+*/
+
+/*
+for(let j = 0; j < arguments.length; j++){
+                for(let i = 0; i < arguments[j].length; i++){
+                    Checking.push(`${arguments[j][i]}: ${false}`)
+                }
+            }
+            return console.log(Checking[0].slice(Checking[0].indexOf(false), Checking[0].length))
+            */
+
+/*
+Object.defineProperty(Checking, arguments[j][i], {value: false, configurable: false})
 */
