535a4a1a98e25b9b165f1e2bfe65ec0000c967d7
XebezeraDefasado <minchoste@gmail.com>
2023-01-26 01:21:49 -0300

    *Tap key* *Tap key* *Tap key*...

--- Home/JS/wordChange.js
+++ Home/JS/wordChange.js
@@ -2,17 +2,22 @@ const button = document.getElementById("button")
 const textbox = document.getElementById("textbox")
 const errorTextbox = document.getElementById("errorTextbox")
 
-button.addEventListener('click', () => {
-    if(!textbox.textContent){
-        var newError = Error()
-        newError.name = '\u274C\u2009ValueError'
-        newError.message = `Please, input a value before. You can't change what doesn't exist, am I right?`
-        errorTextbox.innerHTML = `${newError.name}:<br>${newError.message}`
-        setTimeout(() => {
-            errorTextbox.textContent = null
-        }, 5000)
-        return
-    }
+button.addEventListener('click', function wordChange(){
+    textbox.style.color = "rgba(0, 0, 0, 0)"
+    textbox.contentEditable = false
+
+    function wordReplacing(){
+        if(!textbox.textContent){
+            var newError = Error()
+            newError.name = '\u274C\u2009ValueError'
+            newError.message = `Please, input a value before. You can't change what doesn't exist, am I right?`
+            errorTextbox.innerHTML = `${newError.name}:<br>${newError.message}`
+            setTimeout(() => {
+                textbox.contentEditable = true
+                errorTextbox.textContent = null
+            }, 5000)
+            return
+        }
 
    
         function pickRandom(arg){
@@ -34,70 +39,37 @@ button.addEventListener('click', () => {
             return CheckingF
         }
     
-
-
-    //space = '&nbsp;' AND '\u0020' //- The most fucking important thing, at least at 18:48 1/25/2023
-    let CheckingF = []
+        //space = '&nbsp;' AND '\u0020' //- The most damn important thing, at least at 00:41 1/26/2023
+        
+        let CheckingF = []
     
-    let AditiveConj = [
-        "e",
-        "também",
-        "como também",
-        "além de que",
-        "mais"
-    ]
+        let AditiveConj = [
+            "e",
+            "também",
+            "como também",
+            "além de que",
+            "mais"
+         ]
 
-    createChecking(AditiveConj)
-    
-    if(textbox.innerHTML){
-        function eAditive() {
-            while(textbox.innerHTML.match(/(&nbsp;|\u0020)\u0065(&nbsp;|\u0020)/) !== null){
-                let matcher = textbox.innerHTML.match(/(&nbsp;|\u0020)\u0065(&nbsp;|\u0020)/)[0]
-                let spaceBef = matcher.slice(0, (matcher.indexOf('\u0065')))
-                let spaceAft = matcher.slice(matcher.indexOf('\u0065') +1, matcher.length)
-                textbox.innerHTML = textbox.innerHTML.replace(matcher, `${spaceBef}${pickRandom(AditiveConj)}${spaceAft}`)
+        createChecking(AditiveConj)
+
+        if(textbox.innerHTML){
+            for(e in AditiveConj){
+                let pattern = new RegExp(`(&nbsp;|\u0020)${AditiveConj[e]}(&nbsp;|\u0020)`)
+                 while(textbox.innerHTML.match(pattern)){
+                     let newWord = String(pickRandom(AditiveConj))
+                    textbox.innerHTML = textbox.innerHTML.replace(pattern, `&nbsp;${newWord.toUpperCase()}&nbsp;`)
+                }
+                Object.defineProperty(CheckingF[e], AditiveConj[e], {value: true, writable: true})   
             }
-            CheckingF[0].e = true
-            console.log(CheckingF);
-            return 
+            return
         }
-        eAditive()
     }
-    
+
+    setTimeout(()=>{
+        textbox.style.color = "rgba(0, 0, 0, 1)"
+        wordReplacing();
+        textbox.contentEditable = true
+    }, 1500)
+    return
 })
-
-//"Legacy Code" so you can see my suffering in a step-by-step tutorial
-
-/*
-if(textbox.innerHTML.match(/&nbsp;\u0065&nbsp/) !== null){
-    matcher = textbox.innerHTML.match(/&nbsp;\u0065&nbsp;/)
-    textbox.innerHTML = textbox.innerHTML.replace(/&nbsp;\u0065&nbsp;/, `&nbsp;${pickRandom(AditiveConj)}&nbsp;`)
-}
-*/
-
-/*
-(&nbsp;|\u0020)\u0065(&nbsp;|\u0020)
-*/
-
-//Checking.push(Object.create({i: false}))
-
-/*
-for(let j = 0; j < arguments.length; j++){
-                for(let i = 0; i < arguments[j].length; i++){
-                    Object.defineProperty(Checking, arguments[j][i], {value: false})
-                }
-            }
-*/
-
-/*
-for(let j = 0; j < arguments.length; j++){
-                for(let i = 0; i < arguments[j].length; i++){
-                    Checking.push(`${arguments[j][i]}: ${false}`)
-                }
-            }
-            return console.log(Checking[0].slice(Checking[0].indexOf(false), Checking[0].length))
-            */
-
-/*
-Object.defineProperty(Checking, arguments[j][i], {value: false, configurable: false})
-*/
--- Home/Python/something.py
+++
@@ -1,2 +0,0 @@
-var test = 'a'
-console.log(test)
